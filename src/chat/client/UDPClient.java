0f46e558b73a7320da275b6d8a78c5ae0a3434ff
Kaiki Mello dos Santos <kaiki.mello@gmail.com>
2021-11-29 20:43:00 -0300

    Implementação 1.0 efetuada

--- src/chat/client/UDPClient.java
+++ src/chat/client/UDPClient.java
@@ -1,23 +1,22 @@
 package chat.client;
 
-import javax.sound.midi.Soundbank;
 import java.io.IOException;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class UDPClient {
 
+    public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";
+    public static final String TEXT_RESET = "\u001B[0m";
+    public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";
+
+
     private String nome;
     private InetAddress ip;
     private int porta;
-    private byte[] receberBuffer = new byte[1024];
-    private byte[] enviarBuffer = new byte[1024];
 
     Scanner input = new Scanner(System.in);
-
-    public UDPClient(String nome) {
-        this.nome = nome;
-    }
 
     public UDPClient(InetAddress ip, int porta, String nome) {
         this.ip = ip;
@@ -27,26 +26,47 @@ public class UDPClient {
 
     public void listener(){
         try {
-            DatagramSocket listenerSocket = new DatagramSocket(porta);
+            DatagramSocket listenerSocket = new DatagramSocket(this.porta);
             System.out.println("Aguardando Contato");
 
             while (true){
-                System.out.println(ip + ":" + porta + " | " + this.nome);
-                DatagramPacket pacoteRecebido = new DatagramPacket(receberBuffer, receberBuffer.length);
+                byte[] receberBufferClient = new byte[1024];
+                byte[] enviarBufferClient;
+                /*
+                * Criar um Datagram Packet, recupera o ip, a porta do cliente remetente e a mensagem
+                *
+                * */
+                DatagramPacket pacoteRecebido = new DatagramPacket(receberBufferClient, receberBufferClient.length);
                 listenerSocket.receive(pacoteRecebido);
-                InetAddress ipRemetente = pacoteRecebido.getAddress();
+                InetAddress ipClientRemetente = pacoteRecebido.getAddress();
                 int portaUserRemetente = pacoteRecebido.getPort();
-                System.out.println("A porta do remetente é: "+ portaUserRemetente);
-                String dadoClient = new String(pacoteRecebido.getData());
-                System.out.println("\nCliente: "+ dadoClient);
-                System.out.println("Cliente Listener: ");
+
+                /*
+                * Separa a mensagem do nome do Cliente Remetente
+                * Exibe a mensagem do enviada pelo cliente remetente
+                * */
+                String dadoClient = new String(pacoteRecebido.getData(), StandardCharsets.UTF_8);
+                String mensagemClient = dadoClient.substring(0, dadoClient.indexOf("&"));
+                String nomeCliente = dadoClient.substring(dadoClient.indexOf("&")+1);
+                System.out.println(PURPLE_BOLD_BRIGHT + "Mensagem Recebida de "+ ipClientRemetente.getHostAddress() + " | " + nomeCliente.trim() + ": " + mensagemClient + TEXT_RESET);
+
+                /*
+                * Solicita que o Cliente Listener responda, caso a mensagem seja /bye encerra a
+                * comunicação
+                * */
+                System.out.println(ip.getHostAddress() + " | " + this.nome);
                 String dadoListener = input.nextLine();
                 if(dadoListener.equalsIgnoreCase("/bye")){
                     System.out.println("Conexão encerrada pelo Cliente Listener");
                     break;
                 }
-                enviarBuffer = dadoListener.getBytes();
-                DatagramPacket enviarPacoteListener = new DatagramPacket(enviarBuffer, enviarBuffer.length, ipRemetente, portaUserRemetente);
+
+                /*
+                * Concatena o nome do Cliente Listener a mensagem, converte para bytes, e envia o datagrama
+                * */
+                dadoListener = dadoListener + "&" + this.nome;
+                enviarBufferClient = dadoListener.getBytes();
+                DatagramPacket enviarPacoteListener = new DatagramPacket(enviarBufferClient, enviarBufferClient.length, ipClientRemetente, portaUserRemetente);
                 listenerSocket.send(enviarPacoteListener);
             }
             listenerSocket.close();
@@ -60,20 +80,39 @@ public class UDPClient {
     public void sendMenssage(){
         try {
             DatagramSocket clientSocket = new DatagramSocket();
+
             while (true) {
-                System.out.println(ip + ":" + porta + " | " + this.nome);
+                byte[] bufferEnviarListener;
+                byte[] bufferReceberListener = new byte[1024];
+                System.out.println(ip.getHostAddress() + " | " + this.nome);
                 String dadoCliente = input.nextLine();
+                /*
+                 * Caso a mensagem digitada seja /bye é finalizada a comunicação
+                 */
+
                 if (dadoCliente.equalsIgnoreCase("/bye")){
                     System.out.println("Conexão finalizada pelo Cliente");
                     break;
                 }
-                enviarBuffer = dadoCliente.getBytes();
-                DatagramPacket pacoteEnviar = new DatagramPacket(enviarBuffer, enviarBuffer.length, ip, porta);
+                /*
+                * Concatena o nome do Cliente Listener a mensagem, converte para bytes, e envia o datagrama
+                * */
+                dadoCliente = dadoCliente + "&" + this.nome;
+                bufferEnviarListener = dadoCliente.getBytes();
+                DatagramPacket pacoteEnviar = new DatagramPacket(bufferEnviarListener, bufferEnviarListener.length, ip, porta);
                 clientSocket.send(pacoteEnviar);
-                DatagramPacket pacoteRecebido = new DatagramPacket(receberBuffer, receberBuffer.length);
+
+                /*
+                * Cria um Datagrama Packet, recupera a mensagem, o Ip e o nome
+                * Exibe a mensagem do Listener
+                * */
+                DatagramPacket pacoteRecebido = new DatagramPacket(bufferReceberListener, bufferReceberListener.length);
                 clientSocket.receive(pacoteRecebido);
-                String dadosRecebidosListener = new String(pacoteRecebido.getData());
-                System.out.println("\nListener: " + dadosRecebidosListener);
+                String dadosRecebidosListener = new String(pacoteRecebido.getData(), StandardCharsets.UTF_8);
+                InetAddress ipListener = pacoteRecebido.getAddress();
+                String mensagemListener = dadosRecebidosListener.substring(0, dadosRecebidosListener.indexOf("&"));
+                String nomeListener = dadosRecebidosListener.substring(dadosRecebidosListener.indexOf("&")+1);
+                System.out.println(CYAN_BOLD_BRIGHT+ "Mensagem recebida de "+ ipListener.getHostAddress() + " | " + nomeListener.trim() + ": " + mensagemListener + TEXT_RESET);
             }
             clientSocket.close();
         } catch (UnknownHostException ex1) {
@@ -86,40 +125,5 @@ public class UDPClient {
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
 
-        try {
-            System.out.println("Informe o IP: ");
-            String ipInformado = entrada.next();
-            InetAddress ip = InetAddress.getByName(ipInformado);
-            System.out.println("Informe a porta");
-            int porta = -1;
-            String nome;
-
-            do {
-                System.out.println("A porta informada deve estar entre:");
-                System.out.println("4000 e 6000");
-                porta = entrada.nextInt();
-                System.out.println("Informe seu nome");
-                nome = entrada.next();
-            }while((porta < 4000 || porta > 6000) && nome != null);
-            System.out.println("1 - Cliente Listener");
-            System.out.println("2 - Cliente");
-            int opcaoCliente = entrada.nextInt();
-            System.out.println();
-            boolean flagListener = opcaoCliente == 1;
-            if (flagListener){
-                UDPClient clientListener = new UDPClient(ip, porta, nome);
-                clientListener.listener();
-            }else {
-                UDPClient client = new UDPClient(ip, porta, nome);
-                client.sendMenssage();
-            }
-
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
